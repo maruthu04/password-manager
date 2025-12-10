6094aa7c304b001f88749b240b626103333174bf
maruthu04 <maruthudevendrar@gmail.com>
2025-12-10 14:48:41 +0530

    integrate localStorage, add event handlers, improve password visibility toggle & add sample password table

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-<<<<<<< HEAD
 import Navbar from './components/Navbar'
 import Manager from './components/Manager'
 
@@ -11,17 +10,8 @@ function App() {
     <>
       <Navbar />
       <Manager />
-=======
 
-function App() {
-
-
-  return (
-    <>
-      <h1 text-3xl>Password Manager App begin over here</h1>
->>>>>>> 9f22af4f02ac4fef5ead45a728bc25435a4b0d55
     </>
   )
 }
-
 export default App
--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,14 +1,45 @@
 import React from "react";
+import { useRef, useState, useEffect } from "react";
 
 const Manager = () => {
+  const ref = useRef();
+  const [form, setform] = useState({ site: "", username: "", password: "" });
+  const [passwordArray, setpasswordArray] = useState([]);
+
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+    if (passwords) {
+      setpasswordArray(JSON.parse(passwords));
+    }
+  }, []);
+
+  const showPassword = () => {
+    // console.log(ref.current.src);
+    if (ref.current.src.includes("/eye.png")) {
+      ref.current.src = "/eyecross.png";
+    } else {
+      ref.current.src = "/eye.png";
+    }
+  };
+
+  const savePassword = () => {
+    setpasswordArray([...passwordArray, form]);
+    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
+    console.log([...passwordArray, form]);
+  };
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
-      <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-        <div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-pink-400 opacity-20 blur-[100px]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-pink-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-pink-400 opacity-20 blur-[100px]"></div>
       </div>
 
       <div className="max-w-4xl mx-auto mt-16">
-        <h1 className="text-center text-2xl">
+        <h1 className="text-center text-2xl font-semibold">
           <span className="text-pink-800">&lt;</span>
           Pass
           <span className="text-pink-800">Vault/&gt;</span>
@@ -18,31 +49,81 @@ const Manager = () => {
         </p>
         <div className="text-black flex flex-col p-4 gap-3">
           <input
+            value={form.site}
+            onChange={handleChange}
+            placeholder="Enter Website URL"
             className="rounded-full border border-pink-500 p-1 px-4 m-1"
             type="text"
-            name=""
-            id=""
+            name="site"
+            id="site"
           />
           <div className="flex gap-2">
             <input
+              value={form.username}
+              onChange={handleChange}
+              placeholder="Enter Username/Email"
               className="rounded-full border border-pink-500 p-1 px-4 m-1 w-1/2"
               type="text"
-              name=""
-              id=""
+              name="username"
+              id="username"
             />
-            <input
-              className="rounded-full border border-pink-500 p-1 px-4 m-1 w-1/2"
-              type="text"
-              name=""
-              id=""
-            />
+            <div className="relative w-[47%]">
+              <input
+                value={form.password}
+                onChange={handleChange}
+                placeholder="Enter Password"
+                className="rounded-full border border-pink-500 p-1 px-4 m-1 w-full"
+                type="password"
+                name="password"
+                id="password"
+              />
+              <span
+                className="absolute right-3 top-[22%] cursor-pointer"
+                onClick={showPassword}
+              >
+                <img ref={ref} width={24} src="./eyecross.png" alt="eyecross" />
+              </span>
+            </div>
           </div>
-          <div className="flex justify-center gap-1 bg-pink-300 hover:bg-pink-400 rounded-full mx-auto w-1/5 py-1 mt-3">
+          <div
+            onClick={savePassword}
+            className="flex justify-center gap-1 bg-pink-300 hover:bg-pink-200 rounded-full mx-auto w-1/5 py-1 mt-3 border border-pink-400"
+          >
             <lord-icon
               src="https://cdn.lordicon.com/efxgwrkc.json"
-              trigger="hover"></lord-icon>
+              trigger="hover"
+            ></lord-icon>
             <button>Add Password</button>
           </div>
+        </div>
+        <div className="passwords">
+          <h2>Your Passwords</h2>
+          <table class="table-auto w-full items-center rounded-md overflow-hidden mt-4">
+            <thead className="bg-pink-600 text-white">
+              <tr>
+                <th>Song</th>
+                <th>Artist</th>
+                <th>Year</th>
+              </tr>
+            </thead>
+            <tbody className="bg-pink-100">
+              <tr>
+                <td className="py-2 text-center w-32 border border-white">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                <td className="py-2 text-center w-32 border border-white">Malcolm Lockyer</td>
+                <td className="py-2 text-center w-32 border border-white">1961</td>
+              </tr>
+              <tr>
+                <td className="py-2 text-center w-32 border border-white">Witchy Woman</td>
+                <td className="py-2 text-center w-32 border border-white">The Eagles</td>
+                <td className="py-2 text-center w-32 border border-white">1972</td>
+              </tr>
+              <tr>
+                <td className="py-2 text-center w-32 border border-white">Shining Star</td>
+                <td className="py-2 text-center w-32 border border-white">Earth, Wind, and Fire</td>
+                <td className="py-2 text-center w-32 border border-white">1975</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </>
