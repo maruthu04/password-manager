f879fe2568a6454e50ee87cbbf0b4097e95af39a
maruthu04 <maruthudevendrar@gmail.com>
2025-12-13 12:36:40 +0530

    Fixed Bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
         {/* Right Side */}
         <div className="flex items-center gap-4">
           <button className="hover:scale-110 transition cursor-pointer flex items-center">
-            <img src="/github.svg" width={40} alt="GitHub" />
+            <a href="https://github.com/maruthu04/password-manager" target="_blank"><img src="/github.svg" width={40} alt="GitHub" /></a>
           </button>
 
           {/* Hamburger */}
